dd0ce2bcaf1b135154c314bf6654071f3633b87d
Stanislav Kazikin <kazikinstas@gmail.com>
2021-03-21 22:04:22 +0200

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
-import HomePage from "./views/HomePage.jsx";
-import MoviesPage from "./views/MoviesPage.jsx";
-import MovieDetailsPage from "./views/MovieDetailsPage.jsx";
 import AppBar from "./components/AppBar";
 import routes from "./routes";
+
+const HomePage = lazy(() =>
+  import("./views/HomePage.jsx" /* webpackChunkName: "home-page" */)
+);
+const MoviesPage = lazy(() =>
+  import("./views/MoviesPage.jsx" /* webpackChunkName: "movies-page" */)
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    "./views/MovieDetailsPage.jsx" /* webpackChunkName: "movie-details-page" */
+  )
+);
 
 const App = () => (
   <>
     <AppBar />
-    <Switch>
-      <Route exact path={routes.home} component={HomePage} />
-      <Route path={routes.movieDetails} component={MovieDetailsPage} />
-      <Route path={routes.movie} component={MoviesPage} />
-      <Route component={HomePage} />
-    </Switch>
+    <Suspense fallback={<h1>Загружается...</h1>}>
+      <Switch>
+        <Route exact path={routes.home} component={HomePage} />
+        <Route path={routes.movieDetails} component={MovieDetailsPage} />
+        <Route path={routes.movie} component={MoviesPage} />
+        <Route component={HomePage} />
+      </Switch>
+    </Suspense>
   </>
 );
 
